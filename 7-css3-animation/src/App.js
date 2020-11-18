5f035a08af110f34b03b67b9d0306575f0bc06b6
shenghanqin <shenghanqin@163.com>
2020-11-18 13:09:57 +0800

    update 基础架子

--- 7-css3-animation/src/App.js
+++ 7-css3-animation/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from "react"
+import withUiMode from '@xiaoxili/react-ui-mode-cc'
 import './App.scss';
+import Page from "./components/page";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const { isPCMode } = this.props
+    console.log('isPCMode', isPCMode)
+    return (
+      <Page
+        pageClassName={`page-home ${isPCMode ? 'page-mode-pc' : 'page-mode-mobile'}`}
+        title={'融合响应式设计 React 版 - 小溪里'}
+        isPCMode={isPCMode}
+      >
+  
+      </Page>
+    );
+  }
 }
 
-export default App;
+export default withUiMode({
+  // 区分模式的宽度
+  widthMode: 1000,
+  // iPad 微信恒定为 Mobile UI
+  // isPadWechatMobile: true
+})(App)
+
---
+++ 7-css3-animation/src/components/header/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import classNames from 'classnames/bind';
+import styles from './index.scss';
+
+let cx = classNames.bind(styles);
+
+export default class Page extends React.Component {
+  static defaultProps = {
+    isPCMode: false,
+  }
+
+  render() {
+    const { isPCMode } = this.props
+
+    console.log('header isPCMode :>> ', isPCMode);
+    return (
+      <header>
+        <div className={`header-main ${isPCMode ? 'header-pc' : ''}`}>
+          <h1 className="logo">
+            <a href='/'>
+              小溪里 xiaoxili.com
+            </a>
+          </h1>
+          <nav className="main-nav">
+            <a href='/'>小溪里</a>
+            <a href='/blog' title="小溪里博客">博客</a>
+            <a href='https://github.com/shenghanqin/react-ui-mode-cc' title="react-ui-mode-cc">GitHub</a>
+            <a href='https://www.npmjs.com/package/@xiaoxili/react-ui-mode-cc' title="react-ui-mode-cc">NPM</a>
+          </nav>
+        </div>
+      </header>
+    )
+  }
+}
