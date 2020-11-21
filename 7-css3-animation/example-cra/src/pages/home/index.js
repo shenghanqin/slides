731434861a0f54a7500d63a814f5711f9987d9de
shenghanqin <shenghanqin@163.com>
2020-11-21 20:40:48 +0800

    update 提交功能效果

--- 7-css3-animation/example-cra/src/pages/home/index.js
+++ 7-css3-animation/example-cra/src/pages/home/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import withUiMode from '@xiaoxili/react-ui-mode-cc'
 import './styles.scss';
 import Page from "../../components/page"
 import Data999 from "../../data/status-999.json";
@@ -9,12 +8,10 @@ console.log('Data999', Data999)
 
 class Home extends React.Component {
   render() {
-    const { isPCMode } = this.props
     return (
       <Page
-        pageClassName={`page-home ${isPCMode ? 'page-mode-pc' : 'page-mode-mobile'}`}
+        pageClassName={`page-home`}
         title={'CSS3 动画实战-积分签到'}
-        isPCMode={isPCMode}
         isShowHeader={false}
       >
         <div className="page-main">
@@ -28,10 +25,5 @@ class Home extends React.Component {
   }
 }
 
-export default withUiMode({
-  // 区分模式的宽度
-  widthMode: 1000,
-  // iPad 微信恒定为 Mobile UI
-  // isPadWechatMobile: true
-})(Home)
+export default Home
 
